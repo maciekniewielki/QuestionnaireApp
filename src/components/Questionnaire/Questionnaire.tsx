c1bc31d7a14c602facc2e2feb340f9e50f456227
maciekniewielki <kadlubken5@gmail.com>
2025-01-12 16:03:26 +0100

    Added a thank you screen and data serializaton on submit

--- src/components/Questionnaire/Questionnaire.tsx
+++ src/components/Questionnaire/Questionnaire.tsx
@@ -4,6 +4,7 @@ import { FreeText } from './Fields/FreeText/FreeText';
 import { SingleOption } from './Fields/SingleOption/SingleOption';
 import { Submit } from './Submit/Submit';
 import { QuestionText } from './QuestionText/QuestionText';
+import { useState } from 'react';
 
 export type QuestionnaireProps = {
   headline: string;
@@ -11,27 +12,41 @@ export type QuestionnaireProps = {
 }
 
 export const Questionnaire = ({ headline, questions }: QuestionnaireProps) => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const renderForm = (questions: QuestionProps[]) => {
+    return <form action='#' method='post' onSubmit={onFormSubmitted}>
+      {questions.map((q, index) =>
+        <div key={q.name} className={styles.questionWrapper}>
+          <QuestionText number={index + 1} text={q.question} />
+          {renderField(q)}
+        </div>)}
+      <Submit />
+    </form>
+  }
+
+  const onFormSubmitted = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = Object.fromEntries(questions.map(q => [q.name, event.currentTarget[q.name].value]))
+
+    // TODO send an API request instead of logging
+    console.log("Submitted data:", data);
+    setSubmitted(true);
+  }
+
   return (
     <div className={styles.questionnaire}>
       <h1 className={styles.headline}>{headline}</h1>
-      <form action='#' method='post'>
-        {questions.map((q, index) => 
-        <div key={q.name} className={styles.questionWrapper}>
-          <QuestionText number={index + 1} text={q.question}/>
-          {renderField(q)}
-        </div>)}
-        <Submit />
-      </form>
+      {submitted ? <h2>Thank you for submitting the form</h2> : renderForm(questions)}
     </div>
   )
 }
 
-
 const renderField = (question: QuestionProps) => {
-  switch(question.type) {
+  switch (question.type) {
     case "freeText":
       return <FreeText {...question} />
     case "singleOption":
-      return <SingleOption {...question}/>
+      return <SingleOption {...question} />
   }
 }
